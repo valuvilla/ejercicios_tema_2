37f6c50e384884cd6ea8006272e592a920600b44
valuvilla <valuvp@hotmail.com>
2023-03-11 18:32:23 +0100

    cambios

--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import os
 import re
-import database as db
+import ejercicio2 as ej2
 import helpers
 from colorama import *
 from termcolor import colored, cprint
@@ -10,19 +10,36 @@ def iniciar():
     while True:
         helpers.limpiar_pantalla()
 
-        lineas=(Fore.GREEN+"=="*18)
+        lineas=(Fore.GREEN+"=="*21)
+
         print(lineas)
-        print(colored("  BIENVENIDO AL GESTOR DE CLIENTES  ", 'white', attrs=['bold'], on_color='on_green'))
+        print(colored(("BIENVENIDO AL MENÚ DE PUNTOS Y RECTANGULOS").center(36), 'white', attrs=['bold'], on_color='on_green'))
+        print(colored(Fore.GREEN+("¿Qué desea hacer?").center(42)))
         print( lineas)
-        print(colored(Fore.LIGHTGREEN_EX+"[1]"),"Listar clientes")
-        print(colored(Fore.LIGHTGREEN_EX+"[2]"),"Buscar un cliente")
-        print(colored(Fore.LIGHTGREEN_EX+"[3]"),"Añadir un cliente")
-        print(colored(Fore.LIGHTGREEN_EX+"[4]"),"Modificar un cliente")
-        print(colored(Fore.LIGHTGREEN_EX+"[5]"),"Borrar un cliente")
-        print(colored(Fore.LIGHTGREEN_EX+"[6]"),"Cerrar el Gestor")# CERRAR EL MANAGER
-        print(lineas)
+        print(colored(Fore.LIGHTGREEN_EX+"[1]"),"Crear punto")
+        print(colored(Fore.LIGHTGREEN_EX+"[2]"),"Mostrar cuadrante de un punto")
+        print(colored(Fore.LIGHTGREEN_EX+"[3]"),"Mostrar vector entre dos puntos")
+        print(colored(Fore.LIGHTGREEN_EX+"[4]"),"Mostrar distancia entre dos puntos")
+        print(colored(Fore.LIGHTGREEN_EX+"[5]"),"Crear rectángulo")
+        print(colored(Fore.LIGHTGREEN_EX+"[6]"),"Mostrar base de un rectángulo")
+        print(colored(Fore.LIGHTGREEN_EX+"[7]"), "Mostrar altura de un rectángulo")
+        print(colored(Fore.LIGHTGREEN_EX+"[8]"), "Mostrar área de un rectángulo")
+        print(colored(Fore.LIGHTGREEN_EX+"[9]"), "Salir")
 
         opcion = input(colored(Fore.LIGHTGREEN_EX+"> "))
         helpers.limpiar_pantalla()
 
         if opcion == '1':
+            input_x = int(input("Ingrese la coordenada X: "))
+            input_y = int(input("Ingrese la coordenada Y: "))
+            punto = ej2.Punto(input_x, input_y)
+            print(f"El punto creado es: {ej2.Punto.__str__(punto)}")
+
+
+        if opcion == '9':
+            print(Back.MAGENTA+"SALIENDO\n")
+            break
+
+        input("\nPresiona ENTER para continuar...")
+
+iniciar()
